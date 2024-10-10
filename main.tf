01a926661ec08a7cc918d33de40226149ad3518b
whakkhanna <ronabadeous@yahoo.com.ph>
2024-10-10 14:10:07 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "foo" {
-  ami           = "ami-05fa00d4c63e32376" # us-west-2
+  ami           = "ami-0583d8c7a9c35822c" # us-east1
   instance_type = "t2.micro"
   tags = {
       Name = "TF-Instance"
