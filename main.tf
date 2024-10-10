105f4eff3ea93b9a1860c89324c27b707f84a0f4
whakkhanna <ronabadeous@yahoo.com.ph>
2024-10-10 15:10:59 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
     region = "us-east-1"  
 }
 
-resource "aws_instance" "foo" {
+resource "aws_instance" "rhel" {
   ami           = "ami-0583d8c7a9c35822c" # us-east1
   instance_type = "t2.micro"
   tags = {
